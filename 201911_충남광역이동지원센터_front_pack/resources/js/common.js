5f0f187275a2abfcacaec9f25baac8b87e04a613
parkcsi <36687390+parkcsi@users.noreply.github.com>
2019-11-08 17:47:15 +0900

    커밋
    커밋

--- 201911_충남광역이동지원센터_front_pack/resources/js/common.js
+++ 201911_충남광역이동지원센터_front_pack/resources/js/common.js
@@ -1,4 +1,39 @@
 $(function(){
+	//gnb 호버
+	$(".gnb_wrap .gnb_list > li").hover(function(){
+		$(this).addClass("on");
+		$(".gnb_depth2_wrap").addClass("on").find(".depth2_inner h2 .tit0"+$(this).index()).addClass("on").siblings().removeClass("on");
+	}, function(){
+		$(this).removeClass("on");
+		$(".gnb_depth2_wrap").removeClass("on");
+	});
+	$(".gnb_depth2_wrap .depth2_inner > ul > li").hover(function(){
+		$(".gnb_wrap .gnb_list > li").eq($(this).index()).addClass("on");
+		$(".gnb_depth2_wrap").addClass("on").find(".depth2_inner h2 .tit0"+$(this).index()).addClass("on").siblings().removeClass("on");
+	}, function(){
+		$(".gnb_wrap .gnb_list > li").eq($(this).index()).removeClass("on");
+		$(".gnb_depth2_wrap").removeClass("on");
+	});
+
+	// 메인 슬라이더
+	if($(".main_top_cont").length){
+	var main_visual = new Swiper ('.main_top_cont.swiper-container', {
+		direction: 'horizontal',
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+	});
+};
 
 });
 //Layer Content
