de9388668179c10c8da852d14e6ff89ba661c3ca
parkcsi <qkrckdtjq2@naver.com>
2019-11-09 00:16:51 +0900

    커밋
    커밋

--- 201911_충남광역이동지원센터_front_pack/resources/js/common.js
+++ 201911_충남광역이동지원센터_front_pack/resources/js/common.js
@@ -17,23 +17,53 @@ $(function(){
 
 	// 메인 슬라이더
 	if($(".main_top_cont").length){
-	var main_visual = new Swiper ('.main_top_cont.swiper-container', {
-		direction: 'horizontal',
-		loop: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-		},
-		autoplay: {
-			delay: 5000,
-			disableOnInteraction: false,
-		},
-	});
-};
+		var main_visual = new Swiper ('.main_top_cont.swiper-container', {
+			direction: 'horizontal',
+			loop: true,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+		});
+	};
+
+	// 배너존 슬라이더
+	if($(".info_bnr_zone").length){
+		var bzone_visual = new Swiper ('.info_bnr_zone.swiper-container', {
+			direction: 'horizontal',
+			loop: true,
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'fraction',
+			},
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+		});
+
+		$(document).on("click", ".info_bnr_zone .auto_btn a", function(){
+			if($(this).hasClass("stop")){
+				bzone_visual.autoplay.stop();
+				$(this).removeClass("stop").addClass("start").text("시작");
+			}else{
+				bzone_visual.autoplay.start();
+				$(this).removeClass("start").addClass("stop").text("정지");
+			}
+		});
+	};
 
 });
 //Layer Content
