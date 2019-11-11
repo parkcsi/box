6cd04dd391ed728eb37635143b34c4a1f7517012
parkcsi <qkrckdtjq2@naver.com>
2019-11-11 22:31:43 +0900

    11/11 밤 커밋
    11/11 밤 커밋

--- 201911_충남광역이동지원센터_front_pack/resources/js/common.js
+++ 201911_충남광역이동지원센터_front_pack/resources/js/common.js
@@ -1,18 +1,50 @@
 $(function(){
-	//gnb 호버
-	$(".gnb_wrap .gnb_list > li").hover(function(){
-		$(this).addClass("on");
-		$(".gnb_depth2_wrap").addClass("on").find(".depth2_inner h2 .tit0"+$(this).index()).addClass("on").siblings().removeClass("on");
-	}, function(){
-		$(this).removeClass("on");
-		$(".gnb_depth2_wrap").removeClass("on");
+
+	$(window).resize(function(){
+		if($(window).width() >= 768){
+			$(".gnb_wrap .gnb_list > li").hover(function(){
+				$(this).addClass("on");
+				$(".gnb_depth2_wrap").addClass("on").find(".depth2_inner h2 .tit0"+$(this).index()).addClass("on").siblings().removeClass("on");
+			}, function(){
+				$(this).removeClass("on");
+				$(".gnb_depth2_wrap").removeClass("on");
+			});
+			$(".gnb_depth2_wrap .depth2_inner > ul > li").hover(function(){
+				$(".gnb_wrap .gnb_list > li").eq($(this).index()).addClass("on");
+				$(".gnb_depth2_wrap").addClass("on").find(".depth2_inner h2 .tit0"+$(this).index()).addClass("on").siblings().removeClass("on");
+			}, function(){
+				$(".gnb_wrap .gnb_list > li").eq($(this).index()).removeClass("on");
+				$(".gnb_depth2_wrap").removeClass("on");
+			});
+		};
+		if($(window).width() <= 767){
+			$(".gnb_depth2_wrap.on").removeClass("on");
+			$(".gnb_wrap").find(".on").removeClass("on");
+		};
+		if($(window).width() >= 768){
+			$(".header_inner.m_type").removeClass("m_type");
+		};
 	});
-	$(".gnb_depth2_wrap .depth2_inner > ul > li").hover(function(){
-		$(".gnb_wrap .gnb_list > li").eq($(this).index()).addClass("on");
-		$(".gnb_depth2_wrap").addClass("on").find(".depth2_inner h2 .tit0"+$(this).index()).addClass("on").siblings().removeClass("on");
-	}, function(){
-		$(".gnb_wrap .gnb_list > li").eq($(this).index()).removeClass("on");
-		$(".gnb_depth2_wrap").removeClass("on");
+	$(window).resize();
+	//gnb 호버
+
+	// 모바일 gnb 토글
+	$(".m_gnb_toggle a").click(function(){
+		if($(this).parents(".header_inner").hasClass("m_type")){
+			$(this).parents(".header_inner").removeClass("m_type");
+		}else{
+			$(this).parent().addClass("m_type");
+			$(this).parents(".header_inner").addClass("m_type");
+		};
+	})
+
+	// 모바일 gnb 하위메뉴 토글
+	$(".depth2_tit > a").click(function(){
+		if($(this).parents("li").hasClass("on")){
+			$(this).parents("li").removeClass("on").find(">ul").stop().slideUp();
+		}else{
+			$(this).parents("li").addClass("on").find(">ul").stop().slideDown().parents("li").siblings().removeClass("on").find(">ul").stop().slideUp();
+		};
 	});
 
 	// 메인 슬라이더
