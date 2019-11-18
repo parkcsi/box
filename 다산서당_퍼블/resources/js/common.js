e937201e9a346f6d5626d391b98a4258fab2b5bf
parkcsi <36687390+parkcsi@users.noreply.github.com>
2019-11-18 17:33:59 +0900

    1118 일과커밋
    1118 일과커밋

--- 다산서당_퍼블/resources/js/common.js
+++ 다산서당_퍼블/resources/js/common.js
@@ -15,7 +15,7 @@ $(function(){
 		$(".search_select").removeClass("on");
 	});
 
-	// 검색 필터 선택
+	// 푸터 관련사이트 선택
 	$(document).on("click", ".link_sel > a", function(){
 		if($(this).parent().hasClass("on")){
 			$(this).parent().removeClass("on");
@@ -23,30 +23,6 @@ $(function(){
 			$(this).parent().addClass("on");
 		};
 	});
-
-	$(window).resize(function(){
-		//gnb 호버
-		if($(window).width() >= 768){
-			$(".gnb_wrap .gnb_list > li, .gnb_depth2_wrap").hover(function(){
-				$(this).addClass("on");
-				$(".gnb_depth2_wrap").addClass("on");
-			}, function(){
-				$(this).removeClass("on");
-				$(".gnb_depth2_wrap").removeClass("on");
-			});
-		};
-
-		if($(window).width() <= 767){
-			$(".gnb_depth2_wrap.on").removeClass("on");
-			$(".gnb_wrap").find(".on").removeClass("on");
-			$(".depth2_tit").parent().find(">ul").hide();
-		};
-		if($(window).width() >= 768){
-			$(".header_inner.m_type").removeClass("m_type");
-			$(".depth2_tit").parent().find(">ul").show();
-		};
-	});
-	$(window).resize();
 
 	// 메인 슬라이더
 	if($(".main_top_bnr").length){
@@ -79,10 +55,21 @@ $(function(){
 			direction: 'vertical',
 			loop: false,
 			slidesPerView: 4,
+			slidesPerColumn: 1,
 			navigation: {
 				prevEl: '.top_list_up',
 				nextEl: '.top_list_down',
 			},
+			breakpoints:{
+				640: {
+					slidesPerView: 1,
+				},
+				1299: {
+					direction: 'vertical',
+					slidesPerView: 1,
+					slidesPerColumn: 4,
+				}
+			}
 		});
 	};
 
@@ -93,16 +80,127 @@ $(function(){
 			loop: true,
 			slidesPerView: 4,
 			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
+				nextEl: '.class_list_btn .swiper-button-next',
+				prevEl: '.class_list_btn .swiper-button-prev',
 			},
 		});
 	};
 
-	$(".local_list li a").click(function(){
+	// 공지배너 슬라이더
+	if($(".noti_bnr_wrap").length){
+		var noti_bnr_list = new Swiper ('.noti_bnr_wrap.swiper-container', {
+			direction: 'horizontal',
+			loop: true,
+			navigation: {
+				nextEl: '.noti_bnr_wrap .auto_btn .swiper-button-next',
+				prevEl: '.noti_bnr_wrap .auto_btn .swiper-button-prev',
+			},
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+		});
+		$(document).on("click", ".noti_bnr_wrap .auto_btn a", function(){
+			if($(this).hasClass("stop")){
+				noti_bnr_list.autoplay.stop();
+				$(this).removeClass("stop").addClass("start").text("시작");
+			}else{
+				noti_bnr_list.autoplay.start();
+				$(this).removeClass("start").addClass("stop").text("정지");
+			}
+		});
+	};
+
+	// 관련 사이트 리스트 슬라이드
+	if($(".link_list_inner").length){
+		var link_bnr = new Swiper ('.link_list_inner .swiper-container', {
+			direction: 'horizontal',
+			loop: true,
+			slidesPerView: 5,
+			navigation: {
+				nextEl: '.link_list_inner .swiper-button-next',
+				prevEl: '.link_list_inner .swiper-button-prev',
+			},
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+			breakpoints:{
+				640: {
+					slidesPerView: 1,
+				},
+				950: {
+					slidesPerView: 3,
+				},
+				1300: {
+					slidesPerView: 4,
+				}
+			}
+		});
+		
+		$(document).on("click", ".link_list_inner .auto_btn a", function(){
+			if($(this).hasClass("stop")){
+				link_bnr.autoplay.stop();
+				$(this).removeClass("stop").addClass("start").text("시작");
+			}else{
+				link_bnr.autoplay.start();
+				$(this).removeClass("start").addClass("stop").text("정지");
+			}
+		})
+	};
+
+	// 지역별 강좌 선택
+	$(document).on("click", ".local_list li a", function(){
 		$(".map_btn_wrap > img").attr("src", "../resources/images/content/" + $(this).attr("title") + ".png");
 		$(".map_btn_wrap > img").attr("alt", $(this).text());
 	});
+
+	// 셀렉트 형식 팝업 다른 영역 클릭 시 닫기
+	$(document).mouseup(function(e){
+		var el01 = $(".link_sel_list").parent();
+		var el02 = $(".search_select .select_txt").parent();
+
+		if(el01.has(e.target).length === 0){
+			el01.removeClass("on");
+		}
+		if(el02.has(e.target).length === 0){
+			el02.removeClass("on");
+		}
+	});
+
+	// 공지사항 탭
+	$(document).on("click", ".noti_tab_list > li > a", function(){
+		$(this).parent().addClass("on").siblings().removeClass("on");
+		$(".noti_tab_cont > li").eq($(this).parent().index()).addClass("on").siblings().removeClass("on");
+	});
+
+	$(window).resize(function(){
+		//gnb 호버
+		if($(window).width() >= 768){
+			$(".gnb_wrap .gnb_list > li, .gnb_depth2_wrap").hover(function(){
+				if(!$(this).hasClass("search")){
+					$(this).addClass("on");
+					$(".gnb_depth2_wrap").addClass("on");
+				}
+			}, function(){
+				if(!$(this).hasClass("search")){
+					$(this).removeClass("on");
+					$(".gnb_depth2_wrap").removeClass("on");
+				}
+			});
+		};
+
+		if($(window).width() <= 767){
+			$(".gnb_depth2_wrap.on").removeClass("on");
+			$(".gnb_wrap").find(".on").removeClass("on");
+			$(".depth2_tit").parent().find(">ul").hide();
+		};
+		if($(window).width() >= 768){
+			$(".header_inner.m_type").removeClass("m_type");
+			$(".depth2_tit").parent().find(">ul").show();
+		};
+	});
+	$(window).resize();
 
 	// // 모바일 gnb 토글
 	// $(".m_gnb_toggle a").click(function(){
@@ -122,14 +220,6 @@ $(function(){
 	// 		$(this).parents("li").addClass("on").find(">ul").stop().slideDown().parents("li").siblings().removeClass("on").find(">ul").stop().slideUp();
 	// 	};
 	// });
-
-
-	// top 버튼
-	$(document).on("click", ".floating_btn .top_btn",function(){
-		$('html, body').animate({
-            scrollTop : 0
-        }, 400);
-	})
 
 });
 //Layer Content
