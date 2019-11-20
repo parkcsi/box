7d0fc739acbde91416486874c0bcf61935dc7dbc
parkcsi <36687390+parkcsi@users.noreply.github.com>
2019-11-20 14:33:28 +0900

    기본셋팅
    기본셋팅

---
+++ 충청남도개발공사/resources/js/common.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	
+
+});
+
+//Layer Content
+function layerContShow(thisClass){
+    $('.'+thisClass).show();
+}
+function layerContHide(thisClass){
+    $('.'+thisClass).hide();
+}
