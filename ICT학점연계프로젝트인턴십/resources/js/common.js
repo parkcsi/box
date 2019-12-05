6f65f5ab914076c839af6e91fa836305d1d7bc93
parkcsi <36687390+parkcsi@users.noreply.github.com>
2019-12-05 17:42:11 +0900

    1205 일과
    1205 일과

--- ICT학점연계프로젝트인턴십/resources/js/common.js
+++ ICT학점연계프로젝트인턴십/resources/js/common.js
@@ -1,5 +1,87 @@
 $(function(){
 
+	// 메인관련 슬라이드
+	if($("#container.main").length){
+
+		// 메인배너
+		var main_visual = new Swiper ('.main_top_bnr.swiper-container', {
+			direction: 'horizontal',
+			loop: true,
+			navigation: {
+				nextEl: '.main_top_bnr .swiper-button-next',
+				prevEl: '.main_top_bnr .swiper-button-prev',
+			},
+			pagination: {
+				el: '.main_top_bnr .swiper-pagination',
+				clickable: true,
+			},
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+		});
+
+		// 배너존
+		var main_noti = new Swiper ('.noti_bnr_wrap.swiper-container', {
+			direction: 'horizontal',
+			loop: true,
+			navigation: {
+				nextEl: '.noti_bnr_control .swiper_button_next',
+				prevEl: '.noti_bnr_control .swiper_button_prev',
+			},
+			pagination: {
+				el: ".noti_bnr_control .swiper-pagination",
+				type: "fraction",
+			},
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+		});
+		$(document).on("click", ".noti_bnr_control .auto_btn a", function(){
+			if($(this).hasClass("stop")){
+				main_noti.autoplay.stop();
+				$(this).removeClass("stop").addClass("start").text("시작");
+			}else{
+				main_noti.autoplay.start();
+				$(this).removeClass("start").addClass("stop").text("정지");
+			}
+		});
+
+		// 연수업체목록배너
+		var comp_list = new Swiper ('.main_comp_list.swiper-container', {
+			direction: 'horizontal',
+			loop: true,
+			slidesPerView: 4,
+			slidesPerGroup: 4,
+			loopAdditionalSlides: 5,
+			spaceBetween: 30,
+			pagination: {
+				el: ".comp_btn_wrap .swiper-pagination",
+				type: "fraction",
+			},
+			navigation: {
+				nextEl: '.comp_btn_wrap .swiper_button_next',
+				prevEl: '.comp_btn_wrap .swiper_button_prev',
+			},
+			breakpoints:{
+				1239: {
+					slidesPerView: 3,
+					slidesPerGroup: 3,
+				},
+				767: {
+					slidesPerView: 1,
+					slidesPerGroup: 1,
+				}
+			}
+		});
+	};
+
+	// 메인 공지사항 탭
+	$(".tab_list li a").on("click", function(){
+		$(this).parent().addClass("on").siblings().removeClass("on");
+		$(".noti_tab_cont > li").eq($(this).parent().index()).addClass("on").siblings().removeClass("on");
+	});
 
 });
 
